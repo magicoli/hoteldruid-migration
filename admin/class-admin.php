49c40120baf3bf6d443f1b263d750ab243eedc3b
Olivier van Helden <olivier@van-helden.net>
2022-06-16 18:06:12 -0400

    * added admin notices functions

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -101,3 +101,43 @@ class Hoteldruid_Migration_Admin {
 	}
 
 }
+
+function hmigr_admin_notice($notice, $class='info', $dismissible=true ) {
+  if(empty($notice)) return;
+  if($dismissible) $is_dismissible = 'is-dismissible';
+	if(is_admin()) {
+  add_action( 'admin_notices', function() use ($notice, $class, $is_dismissible) {
+    ?>
+    <div class="notice notice-<?=$class?> <?=$is_dismissible?>">
+        <p><strong><?php echo HMIGR_PLUGIN_NAME; ?></strong>: <?php _e( $notice, 'band-tools' ); ?></p>
+    </div>
+    <?php
+  } );
+	} else {
+		hmigr_transient_admin_notice($notice, $class, $dismissible, __FUNCTION__);
+	}
+}
+
+function hmigr_transient_admin_notice( $notice, $class='info', $dismissible=true, $key = NULL ) {
+	$transient_key = sanitize_title(HMIGR_PLUGIN_NAME . '_hmigr_transient_admin_notices');
+
+	$queue = get_transient( $transient_key );
+	if(!is_array($queue)) $queue = array($queue);
+
+	$hash = hash('md5', $notice);
+	$queue[$hash] = array('notice' => $notice, 'class' => $class, 'dismissible' => $dismissible);
+	set_transient( $transient_key, $queue );
+}
+
+function hmigr_get_transient_admin_notices() {
+	if(!is_admin()) return;
+	$transient_key = sanitize_title(HMIGR_PLUGIN_NAME . '_hmigr_transient_admin_notices');
+	$queue = get_transient( $transient_key );
+	if(!is_array($queue)) $queue = array($queue);
+	foreach($queue as $key => $notice) {
+		if(!is_array($notice)) continue;
+		hmigr_admin_notice($notice['notice'], $notice['class'], $notice['dismissible'] );
+	}
+	delete_transient( $transient_key );
+}
+add_action('admin_head', 'hmigr_get_transient_admin_notices');
