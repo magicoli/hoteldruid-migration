50605c261893d016aafc5943dc7a010dd577b48d
Olivier van Helden <olivier@van-helden.net>
2022-06-16 13:45:32 -0400

    added metabox library

--- hoteldruid-migration.php
+++ hoteldruid-migration.php
@@ -54,6 +54,7 @@ register_deactivation_hook( __FILE__, 'deactivate_hoteldruid_migration' );
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
+require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 require plugin_dir_path( __FILE__ ) . 'includes/class.php';
 
 /**
