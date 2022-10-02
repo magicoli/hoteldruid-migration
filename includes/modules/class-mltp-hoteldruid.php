0068da2786180d7d83e6e1b58f8cb11863dc7372
Olivier van Helden <olivier@van-helden.net>
2022-10-02 18:21:30 -0400

    cleanup

--- includes/modules/class-mltp-hoteldruid.php
+++ includes/modules/class-mltp-hoteldruid.php
@@ -47,15 +47,21 @@ class Mltp_HotelDruid extends Mltp_Modules {
 	 * @since    0.1.0
 	 */
 	public function __construct() {
-    $this->module = 'hoteldruid';
-    $this->name = 'HotelDruid';
+		$this->module = 'hoteldruid';
+		$this->name   = 'HotelDruid';
 
-		$this->clienti = get_transient('hoteldruid_migration_table_clients');
-		if(empty($this->clienti)) $this->clienti = [];
-	  $this->bookings = get_transient('hoteldruid_migration_table_bookings');
-		if(empty($this->bookings)) $this->bookings = [];
-	  $this->appartementi = get_transient('hoteldruid_migration_table_accommodations');
-		if(empty($this->appartementi)) $this->appartementi = [];
+		$this->clienti = get_transient( 'hoteldruid_migration_table_clients' );
+		if ( empty( $this->clienti ) ) {
+			$this->clienti = array();
+		}
+		$this->prenota = get_transient( 'hoteldruid_migration_table_bookings' );
+		if ( empty( $this->prenota ) ) {
+			$this->prenota = array();
+		}
+		$this->appartementi = get_transient( 'hoteldruid_migration_table_accommodations' );
+		if ( empty( $this->appartementi ) ) {
+			$this->appartementi = array();
+		}
 		// register_activation_hook( MULTIPASS_FILE, __CLASS__ . '::activate' );
 		// register_deactivation_hook( MULTIPASS_FILE, __CLASS__ . '::deactivate' );
 	}
@@ -69,15 +75,15 @@ class Mltp_HotelDruid extends Mltp_Modules {
 
 		$this->actions = array(
 			// array(
-			// 	'hook'          => 'wp_insert_post',
-			// 	'callback'      => 'wp_insert_post_action',
-			// 	'accepted_args' => 3,
+			// 'hook'          => 'wp_insert_post',
+			// 'callback'      => 'wp_insert_post_action',
+			// 'accepted_args' => 3,
 			// ),
 			// array(
-			// 	// 'hook'          => 'save_post_shop_order',
-			// 	'hook'          => 'save_post', // use save_post because save_post_prestation_item is fired before actual save and meta values are not yet updated.
-			// 	'callback'      => 'save_post_action',
-			// 	'accepted_args' => 3,
+			// 'hook'          => 'save_post_shop_order',
+			// 'hook'          => 'save_post', // use save_post because save_post_prestation_item is fired before actual save and meta values are not yet updated.
+			// 'callback'      => 'save_post_action',
+			// 'accepted_args' => 3,
 			// ),
 		);
 
@@ -86,10 +92,10 @@ class Mltp_HotelDruid extends Mltp_Modules {
 				'hook'     => 'mb_settings_pages',
 				'callback' => 'register_settings_pages',
 			),
-      array(
-      	'hook'     => 'rwmb_meta_boxes',
-      	'callback' => 'register_settings_fields',
-      ),
+			array(
+				'hook'     => 'rwmb_meta_boxes',
+				'callback' => 'register_settings_fields',
+			),
 			array(
 				'hook'     => 'rwmb_meta_boxes',
 				'callback' => 'register_fields',
@@ -98,28 +104,17 @@ class Mltp_HotelDruid extends Mltp_Modules {
 				'hook'     => 'multipass_register_terms_prestation-item-source',
 				'callback' => 'register_sources_filter',
 			),
-      //
+		//
 			// array(
-			// 	'hook'     => 'multipass_update_resource_title',
-			// 	'callback' => 'update_resource_title',
+			// 'hook'     => 'multipass_update_resource_title',
+			// 'callback' => 'update_resource_title',
 			// ),
-      //
-      //
+		//
+		//
 			// array(
-			// 	'hook'          => 'manage_prestation_posts_custom_column',
-			// 	'callback'      => 'prestations_columns_display',
-			// 	'accepted_args' => 2,
-			// ),
-			// array(
-			// 	'hook'          => 'manage_shop_order_posts_custom_column',
-			// 	'callback'      => 'shop_orders_columns_display',
-			// 	'accepted_args' => 2,
-			// ),
-      //
-			// array(
-			// 	'hook'          => 'hoteldruid_order_data_store_cpt_get_orders_query',
-			// 	'callback'      => 'wc_get_orders_handle_prestation_id',
-			// 	'accepted_args' => 2,
+			// 'hook'          => 'manage_prestation_posts_custom_column',
+			// 'callback'      => 'prestations_columns_display',
+			// 'accepted_args' => 2,
 			// ),
 		);
 
@@ -145,7 +140,7 @@ class Mltp_HotelDruid extends Mltp_Modules {
 		// self::sync_now();
 	}
 
-  /**
+	/**
 	 * Add HotelDruid tab to settings page. 'HotelDruid' string is intentionally
 	 * left not translatable as it is a brand name.
 	 *
@@ -166,29 +161,29 @@ class Mltp_HotelDruid extends Mltp_Modules {
 			'id'             => 'multipass-hoteldruid-settings',
 			'settings_pages' => array( 'multipass-settings' ),
 			'tab'            => 'hoteldruid',
-      'fields'         => array(
+			'fields'         => array(
 				array(
-					'name' => __('Import data', 'hoteldruid-migration'),
-					'type' => 'custom_html',
-					'callback' => [ $this, 'render_import_data' ],
+					'name'     => __( 'Import data', 'hoteldruid-migration' ),
+					'type'     => 'custom_html',
+					'callback' => array( $this, 'render_import_data' ),
 				),
 				array(
-					'name' => __('Resources', 'multipass'),
-					'id' => 'resources',
-					'type' => 'group',
-					'fields' => $this->get_appartementi_resource_field(),
-					'sanitize_callback' => [ $this, 'sanitize_resource' ],
+					'name'              => __( 'Resources', 'multipass' ),
+					'id'                => 'resources',
+					'type'              => 'group',
+					'fields'            => $this->resource_group_fields(),
+					'sanitize_callback' => array( $this, 'sanitize_resource' ),
 				),
-        array(
-          'name'              => __( 'Synchronize now', 'multipass' ),
-          'id'                => $prefix . 'sync_now',
-          'type'              => 'switch',
-          'desc'              => __( 'Sync orders and prestations, create prestation if none exist. Only useful after plugin activation or if out of sync.', 'multipass' ),
-          'style'             => 'rounded',
-          'sanitize_callback' => [ $this, 'sync_now_sanitize_callback' ],
-          'save_field'        => false,
-        ),
-      ),
+				array(
+					'name'              => __( 'Synchronize now', 'multipass' ),
+					'id'                => $prefix . 'sync_now',
+					'type'              => 'switch',
+					'desc'              => __( 'Sync orders and prestations, create prestation if none exist. Only useful after plugin activation or if out of sync.', 'multipass' ),
+					'style'             => 'rounded',
+					'sanitize_callback' => array( $this, 'sync_now_sanitize_callback' ),
+					'save_field'        => false,
+				),
+			),
 		);
 
 		return $meta_boxes;
@@ -215,34 +210,14 @@ class Mltp_HotelDruid extends Mltp_Modules {
 		// Order info on prestation-item
 
 		// Prestation info on WC Orders
-		$prefix       = 'prestation_';
-		$meta_boxes[] = array(
-			'title'      => __( 'Prestation', 'multipass' ),
-			'id'         => 'prestation-hoteldruid-order',
-			'post_types' => array( 'shop_order' ),
-			'context'    => 'side',
-			'fields'     => array(
-				array(
-					// 'name'       => __('Prestation', 'multipass' ),
-					'id'         => $prefix . 'id',
-					'type'       => 'post',
-					'post_type'  => array( 'prestation' ),
-					'field_type' => 'select_advanced',
-				),
-				array(
-					'id'       => $prefix . 'prestation_link',
-					'type'     => 'custom_html',
-					'callback' => __CLASS__ . '::display_prestation_link',
-				),
-			),
-		);
+		$prefix = 'prestation_';
 
 		$meta_boxes['resources']['fields'][] = array(
 			'name'          => __( 'HotelDruid', 'multipass' ),
 			'id'            => 'resource_hoteldruid_id',
 			'type'          => 'select_advanced',
-			'options' => $this->get_appartementi_options(),
-			'placeholder' => __('Select a lodging', 'multipass'),
+			'options'       => $this->get_appartementi_options(),
+			'placeholder'   => __( 'Select a lodging', 'multipass' ),
 			'admin_columns' => array(
 				'position'   => 'before date',
 				'sort'       => true,
@@ -255,57 +230,56 @@ class Mltp_HotelDruid extends Mltp_Modules {
 	}
 
 	function get_appartementi_options() {
-		$options = [];
-		foreach($this->appartementi as $key => $value) {
-			$options[$key] = $key;
+		$options = array();
+		foreach ( $this->appartementi as $key => $value ) {
+			$options[ $key ] = $key;
 		}
 		return $options;
 	}
 
-	function get_appartementi_resource_field() {
-		$fields = [];
-		foreach($this->appartementi as $key => $value) {
+	function resource_group_fields() {
+		$fields = array();
+		foreach ( $this->appartementi as $key => $value ) {
 			$fields[] = array(
-				'name' => $key,
-				'id' => $key,
-				'type'          => 'post',
-				'post_type'     => array( 'mp_resource' ),
-				'field_type'    => 'select_advanced',
-				'placeholder' => __('Do not import', 'multipass'),
-				'size' => 5,
+				'name'        => $key,
+				'id'          => $key,
+				'type'        => 'post',
+				'post_type'   => array( 'mp_resource' ),
+				'field_type'  => 'select_advanced',
+				'placeholder' => __( 'Do not import', 'multipass' ),
+				'size'        => 5,
 			);
 		}
 		return $fields;
 	}
 
-	function sanitize_resource($values, $field, $old_value) {
+	function sanitize_resource( $values, $field, $old_value ) {
 		// $options = ;
-		$options = array_fill_keys(array_keys($this->get_appartementi_options()), null);
-		$options = array_replace($options, $values);
+		$options = array_fill_keys( array_keys( $this->get_appartementi_options() ), null );
+		$options = array_replace( $options, $values );
 		// error_log("options " . print_r($options, true));
 
-		foreach($options as $idappartementi=>$resource_id) {
-			$query = $this->get_resources($idappartementi);
+		foreach ( $options as $idappartementi => $resource_id ) {
+			$query = $this->get_resources( $idappartementi );
 
 			while ( $query->have_posts() ) {
 				$query->the_post();
 				$post_id = get_the_ID();
-				if($post_id !== $resource_id) {
-					update_post_meta($post_id, 'resource_hoteldruid_id', null);
+				if ( $post_id !== $resource_id ) {
+					update_post_meta( $post_id, 'resource_hoteldruid_id', null );
 				}
 			}
 
-			if(!empty($resource_id)) {
+			if ( ! empty( $resource_id ) ) {
 				// add reference to resource
-				$resource = new Mltp_Resource($resource_id);
-				if($resource) {
-					update_post_meta($resource->id, 'resource_hoteldruid_id', $idappartementi);
+				$resource = new Mltp_Resource( $resource_id );
+				if ( $resource ) {
+					update_post_meta( $resource->id, 'resource_hoteldruid_id', $idappartementi );
 				} else {
 					$resource_id = null;
 				}
 				// error_log("$idappartementi = $resource_id resource->id $resource->id " . print_r($meta, true));
 			}
-
 		}
 
 		return $values;
@@ -330,7 +304,7 @@ class Mltp_HotelDruid extends Mltp_Modules {
 		if ( empty( $idappartementi ) ) {
 			return;
 		}
-		$query = $this->get_resources($idappartementi);
+		$query = $this->get_resources( $idappartementi );
 		if ( $query->have_posts() ) {
 			$query->the_post();
 			return get_the_ID();
@@ -344,9 +318,9 @@ class Mltp_HotelDruid extends Mltp_Modules {
 			<li>%s clients</li>
 			<li>%s bookings</li>
 			</ul>',
-			count($this->appartementi),
-			count($this->clienti),
-			count($this->bookings),
+			count( $this->appartementi ),
+			count( $this->clienti ),
+			count( $this->prenota ),
 		);
 		return $html;
 	}
@@ -354,30 +328,6 @@ class Mltp_HotelDruid extends Mltp_Modules {
 	function register_sources_filter( $sources ) {
 		$sources['hoteldruid'] = 'HotelDruid';
 		return $sources;
-	}
-
-	function wc_get_orders_handle_prestation_id( $query, $query_vars ) {
-		if ( ! empty( $query_vars['prestation_id'] ) ) {
-			$query['meta_query'][] = array(
-				'key'   => 'prestation_id',
-				'value' => esc_attr( $query_vars['prestation_id'] ),
-			);
-		}
-		return $query;
-	}
-
-	function display_prestation_link( $arg = null, $field = null ) {
-		global $post;
-		$prestation_id = get_post_meta( $post->ID, 'prestation_id', true );
-		$link          = get_edit_post_link( $prestation_id );
-
-		if ( ! empty( $link ) ) {
-			echo sprintf(
-				'<a href="%s">%s</a>',
-				$link,
-				__( 'View prestation', 'multipass' ),
-			);
-		}
 	}
 
 	function get_related_links( $post_id, $relation_id, $direction ) {
